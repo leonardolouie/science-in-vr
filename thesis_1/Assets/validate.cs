ac4419731c8d1778f9cdb55bc6a3dc4d61868309
Aj <42228140+sleepingburger@users.noreply.github.com>
2018-08-15 14:58:21 +0800

    change 6

---
+++ thesis_1/Assets/validate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class validate : MonoBehaviour {
+
+
+	public Image def,solar,anatomy;
+	Image a;
+	public GameObject mainPanel,loginPanel;
+	public InputField user,pass;
+	// Use this for initialization
+	public void test(){
+		if (user.text == "username" && pass.text == "password") {
+			mainPanel.SetActive (true);
+			loginPanel.SetActive (false);
+		} else
+			Debug.Log ("OOPS");
+	}
+	void Awake(){
+		a = def;
+	}
+}

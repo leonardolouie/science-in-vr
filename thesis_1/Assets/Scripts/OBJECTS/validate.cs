3449df699d20d674a6ddf41dc4ea985108cf02d0
leonardolouie <leonardolouie30@gmail.com>
2018-09-18 10:30:50 -0400

    qweqeqweqeqeqwe
    qweqeqwe

--- thesis_1/Assets/Scripts/OBJECTS/validate.cs
+++ thesis_1/Assets/Scripts/OBJECTS/validate.cs
@@ -4,29 +4,11 @@ using UnityEngine;
 using UnityEngine.UI;
 public class validate : MonoBehaviour {
 
-	bool aa = true;
-	public Text try2;
-	public Image def,solar,anatomy,try1;
+	public Image def,solar,anatomy;
 	Image a;
 	public GameObject mainPanel,loginPanel;
 	public InputField user,pass;
 	// Use this for initialization
-	void Update(){
-		if (try1.fillAmount != 0f && aa) {
-			try1.fillAmount -= 0.5f * Time.deltaTime;
-			if (try1.fillAmount == 0f)
-				aa = false;
-		} else {
-			try1.fillAmount += 0.5f * Time.deltaTime;
-			if (try1.fillAmount == 1f)
-				aa = true;
-		}
-
-		//if (try1.fillAmount < 1f)
-			//try1.fillAmount += 0.01f;
-
-		try2.text = (try1.fillAmount * 100f).ToString ();
-	}
 	public void test(){
 		if (user.text == "123456" && pass.text == "password") {
 			mainPanel.SetActive (true);

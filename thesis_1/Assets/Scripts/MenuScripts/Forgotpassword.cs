bb75cf110cc3f653e0a240abdbf7fd6fc3df37b0
leonardolouie <leonardolouie30@gmail.com>
2018-10-02 07:05:51 -0400

    some
    qweqee

--- thesis_1/Assets/Scripts/MenuScripts/Forgotpassword.cs
+++ thesis_1/Assets/Scripts/MenuScripts/Forgotpassword.cs
@@ -16,7 +16,7 @@ public class Forgotpassword : MonoBehaviour {
 	public GameObject loginPanel, changePanel, checkPanel;
 
 	public Text errorfield;
-
+	public Text accountname;
 
 
 
@@ -110,15 +110,7 @@ public class Forgotpassword : MonoBehaviour {
 					//reponse details			
 					if (userDetail.status == 1) 
 					{    
-						lblLoader.text = "Credentials Found!....";
-						yield return new WaitForSeconds (2f);
-
-						checkPanel.SetActive (false);
-						changePanel.SetActive (true);
-
-
-					
-						canvasLoad.SetActive (false);
+						StartCoroutine (fetch (lrn));
 
 
 					} 
@@ -135,6 +127,175 @@ public class Forgotpassword : MonoBehaviour {
 
 
 	}
+
+
+
+	IEnumerator fetch(string lrn)
+	{
+
+		string SetUrl = "https://scivre.herokuapp.com/api/webapifetchname";
+
+		if(Validation1.checkConnectionfail() == true)
+		{
+			Debug.Log ("Error: Internet Connection");
+			canvasLoad.SetActive (false);
+
+		} 
+		else 
+		{
+			WWWForm form = new WWWForm ();
+			form.AddField ("id", lrn);
+
+			using (UnityWebRequest www = UnityWebRequest.Post (SetUrl, form)) 
+			{
+				canvasLoad.SetActive (true);
+				lblLoader.text = "Checking Credentials";
+				www.chunkedTransfer = false;
+				yield return www.SendWebRequest();
+
+				if (www.error != null)
+				{
+					Debug.Log("Error webserver request error: "+ www.error);
+				}
+				else
+				{ 
+					Debug.Log ("Response" + www.downloadHandler.text);
+					Validation1.UserData userData= JsonUtility.FromJson<Validation1.UserData> (www.downloadHandler.text);
+					//reponse detail
+
+					accountname.text = "Account Name: " + userData.first_name + " " + userData.last_name;
+
+					lblLoader.text = "Credentials Found!....";
+					yield return new WaitForSeconds (3f);
+
+					checkPanel.SetActive (false);
+					changePanel.SetActive (true);
+					canvasLoad.SetActive (false);
+
+
+				}
+			}
+		}
+	}
+
+
+
+
+
+
+
+	public InputField retype;
+	public InputField password;
+
+
+
+
+
+	public void Change()
+	{
+
+
+		if (retype.text != "" && password.text != "")
+		{
+
+			if (Validation1.CheckPasswordMatch (password.text, retype.text) == true) {
+
+				StartCoroutine (Changepass (password.text, username.text));
+			} else {
+
+				errorfield.text = "Password did not match";
+			}
+
+		}
+
+		else 
+		{
+
+         	errorfield.text = "All fields are required";
+		}
+
+	}
+
+
+	IEnumerator  Changepass(string password, string name)
+	{
+
+
+		string ChangepassUrl="https://scivre.herokuapp.com/api/webapichangepass";
+
+		canvasLoad.SetActive (true);
+		lblLoader.text = "Sending Request to webserver";
+
+		//	first if checking internet connection ang web serve response pare
+		if (Validation1.checkConnectionfail() == true) 
+		{
+
+			errorfield.text = "Error: Internet Connection";
+			canvasLoad.SetActive(false);
+		} 
+		else 
+
+		{
+			//Checking web server response
+			WWWForm form = new WWWForm ();
+			form.AddField ("password", password);
+			form.AddField ("name", name);
+
+
+
+			using (UnityWebRequest www = UnityWebRequest.Post (ChangepassUrl, form)) 
+			{
+				www.chunkedTransfer = false;
+				yield return www.SendWebRequest();
+
+				Debug.Log (www.error+" ");
+				if (www.error != null)
+				{
+					errorfield.text = "Error webserver request error: "+ www.error;
+				}
+				else
+				{ 
+					Debug.Log ("Response" + www.downloadHandler.text);
+
+					Validation1.UserDetail userDetail = JsonUtility.FromJson<Validation1.UserDetail> (www.downloadHandler.text);
+					//reponse details			
+					if (userDetail.status == 1) 
+					{
+						lblLoader.text = "Password Successfully Updated";
+						yield return new WaitForSeconds (3f);
+						errorfield.text = userDetail.message;
+						canvasLoad.SetActive(false);
+					  
+
+
+					
+						changePanel.SetActive (false);
+
+						loginPanel.SetActive (true);
+
+
+
+
+
+
+
+					} 
+					else
+					{
+						errorfield.text = www.downloadHandler.text;
+					}
+
+
+
+				}
+
+			}
+
+
+		}
+	}	
+
+
 
 
 }

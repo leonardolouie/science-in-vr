7a6b01b9023562a33779b3161e66481a2d41dde3
leonardolouie <leonardolouie30@gmail.com>
2018-10-02 09:10:03 -0400

    qweqweqeqe
    qweqeqeqeqe

--- thesis_1/Assets/Scripts/MenuScripts/changepass.cs
+++ thesis_1/Assets/Scripts/MenuScripts/changepass.cs
@@ -15,16 +15,48 @@ public class changepass : MonoBehaviour {
 
 
 
+
+
+
+	public GameObject canvasLoad;
+	public Text lblLoader;
+	public RectTransform main;
+	public float timeStep;
+	public float oneStepAngle;
+
+
+	float startTime;
+
+
+
+
+
+	void Update(){
+		if (Time.time - startTime >= timeStep) {
+			Vector3 angle = main.localEulerAngles;
+			angle.z += oneStepAngle;
+
+			main.localEulerAngles = angle;
+
+			startTime = Time.time;
+		}
+	}
+
+
+
+
+
 	public void Change()
 	{
-
+		string username = PlayerPrefs.GetString ("name");
+		Debug.Log (username);
 
 		if (retype.text != "" && password.text != "")
 		{
 
 			if (Validation1.CheckPasswordMatch (password.text, retype.text) == true) {
-
-				StartCoroutine (Changepass (password.text));
+				
+				StartCoroutine (Changepass (password.text, username));
 			} else {
 
 				errorfield.text = "Password did not match";
@@ -41,51 +73,60 @@ public class changepass : MonoBehaviour {
 	}
 
 
-	IEnumerator  Changepass(string password)
+	IEnumerator  Changepass(string passworda, string username)
 	{
 
-		errorfield.text = "";
 
+		string ChangepassUrl="https://scivre.herokuapp.com/api/webapichangepass";
+
+		canvasLoad.SetActive (true);
+		lblLoader.text = "Sending Request to webserver";
 
 		//	first if checking internet connection ang web serve response pare
 		if (Validation1.checkConnectionfail() == true) 
 		{
 
 			errorfield.text = "Error: Internet Connection";
+			canvasLoad.SetActive(false);
 		} 
 		else 
 
 		{
-		//Checking web server response
-		WWWForm form = new WWWForm ();
-
-
-		form.AddField ("password", password);
-	     
-
+			//Checking web server response
+			WWWForm form = new WWWForm ();
+			Debug.Log (password);
+			form.AddField ("password", passworda);
+			form.AddField ("name", username );
 
 
 
-		using (UnityWebRequest www = UnityWebRequest.Post (ChangepassUrl, form)) 
-		{
-			www.chunkedTransfer = false;
-			yield return www.SendWebRequest();
-
-			Debug.Log (www.error+" ");
-			if (www.error != null)
+			using (UnityWebRequest www = UnityWebRequest.Post (ChangepassUrl, form)) 
 			{
-				errorfield.text = "Error webserver request error: "+ www.error;
-			}
-			else
-			{ 
+				www.chunkedTransfer = false;
+				yield return www.SendWebRequest();
+
+				Debug.Log (www.error+" ");
+				if (www.error != null)
+				{
+					errorfield.text = "Error webserver request error: "+ www.error;
+				}
+				else
+				{ 
 				Debug.Log ("Response" + www.downloadHandler.text);
 
 				Validation1.UserDetail userDetail = JsonUtility.FromJson<Validation1.UserDetail> (www.downloadHandler.text);
 				//reponse details			
 				if (userDetail.status == 1) 
 				{
-					errorfield.text = userDetail.message;
+					lblLoader.text = userDetail.message;
+				    yield return new WaitForSeconds (2f); 
+					canvasLoad.SetActive(false);
 
+						password.text = "";
+						retype.text = "";
+				
+					
+					
 
 
 
@@ -97,9 +138,9 @@ public class changepass : MonoBehaviour {
 
 
 
-			}
+				}
 
-		}
+			}
 
 
 		}

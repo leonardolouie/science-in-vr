f58c02d4dde4870a2d134a196fea46614564c128
Aj <42228140+sleepingburger@users.noreply.github.com>
2018-10-13 06:35:10 +0800

    qweqe
    qwewqeqwe

--- thesis_1/Assets/Lomenu UI/Scripts/PanelAnim.cs
+++ thesis_1/Assets/Lomenu UI/Scripts/PanelAnim.cs
@@ -5,6 +5,15 @@ using System.Collections.Generic;
 
 public class PanelAnim : MonoBehaviour {
 
+
+	public GameObject start;
+	public Image spr;
+	int index = 0;
+	public string[] infos;
+	public Text textInfo;
+	public RectTransform[] locationsAnim;
+	public Animator tutorialAnimator;
+
 	public List<GameObject> pages = new List<GameObject>();
 	int currentPanelIndex = 0;
 	public GameObject currentPanel;
@@ -14,17 +23,54 @@ public class PanelAnim : MonoBehaviour {
 	public bool fadeIn = false;
 	public float fadeFactor = 8f;
 
+
+
+	void Start(){
+		if (PlayerPrefs.GetInt ("tutorial", 0) == 0) {
+			tutorialAnim ();
+			PlayerPrefs.SetInt ("tutorial", 1);
+		}
+		else 
+			start.transform.parent.gameObject.SetActive (false);
+		
+	}
 	void Update ()
 	{
+			
 			if (fadeOut) {
 				canvasGroup.alpha -= fadeFactor * Time.deltaTime;
 			}
 			if (fadeIn) {
 				canvasGroup.alpha += fadeFactor * Time.deltaTime;
 			}
-		
-			
 	}
+
+	public void tutorialAnim(){
+		if (index < 4) {
+			StartCoroutine (changePos ());
+		} else {
+			start.SetActive (true);
+			spr.transform.parent.gameObject.SetActive (false);
+		}
+	}
+
+	IEnumerator changePos(){
+
+
+		spr.CrossFadeAlpha (0f, .5f,false);
+		spr.CrossFadeAlpha (1f, .5f,false);
+		tutorialAnimator.SetTrigger ("tap");
+		yield return new WaitForSeconds (tutorialAnimator.GetCurrentAnimatorStateInfo (0).length - .15f);
+		spr.gameObject.transform.parent.GetComponent<RectTransform> ().anchoredPosition = locationsAnim [index].anchoredPosition;
+		textInfo.text = infos [index];
+		if (index < 4) {
+			newPanel (index);
+			index++;
+		}
+	}
+
+
+
 
 	public void newPanel(int newPage)
 	{
---
+++ thesis_1/Assets/dashPanel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class dashPanel : MonoBehaviour {
+
+
+	public GameObject[] progress;
+	int index;
+	float timeBetween = 5f, nextTime = 3f;
+	// Use this for initialization
+	void Start () {
+		//StartCoroutine (loadProgress ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > timeBetween) {
+			
+			progress[index].transform.GetChild(2).gameObject.GetComponent<Image>().fillAmount += 1f / 3f * Time.deltaTime;
+			if (Time.time > nextTime) {
+				index++;
+				timeBetween += Time.time;
+			}
+		}
+	}
+
+
+}
